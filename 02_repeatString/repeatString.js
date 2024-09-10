d5426ef3d25f27abfa9c9c49c29d1caa0818ba2c
Philipp Michael Arbeitstein <philippar@edu.aau.at>
2024-09-10 13:45:40 +0200

    Repeat strings

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(inputString, repetitions) {
+    if(repetitions < 0) {
+        return "ERROR";
+    }
+    let output = "";
+    let i = 0;
+    while(i < repetitions) {
+        output += inputString;
+        i++;
+    }
+    return output;
 };
 
 // Do not edit below this line
