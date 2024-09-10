b013eaff5a01986ba80e4a265c2937534365afa7
Philipp Michael Arbeitstein <philippar@edu.aau.at>
2024-09-10 14:54:37 +0200

    Temp Conversions

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(kelvin) {
+  return kelvin == 32 ? 0 : Number(((kelvin-32)*(5/9)).toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celcius) {
+  return celcius == 0 ? 32 : Number((celcius*(9/5)+32).toFixed(1));
 };
 
 // Do not edit below this line
